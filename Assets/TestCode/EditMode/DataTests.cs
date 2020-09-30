7cba222bff5f0b5a1b8e74e29d451d979bf352f9
Kamrul Islam <md.kamrul.islam@g.bracu.ac.bd>
2020-09-30 23:22:13 +0600

    Final Push

--- Assets/TestCode/EditMode/DataTests.cs
+++ Assets/TestCode/EditMode/DataTests.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-namespace Unit_Test.EditMode
+namespace TestCode.EditMode
 {
     public class DataTests
     {
--- Assets/TestCode/PlayMode/HighScoreInterfaceTests.cs
+++ Assets/TestCode/PlayMode/HighScoreInterfaceTests.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-namespace Unit_Test.PlayMode
+namespace TestCode.PlayMode
 {
     public class HighScoreInterfaceTests
     {
